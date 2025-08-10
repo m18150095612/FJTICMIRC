17a8a8fc43a388db4489dea72f2692b5d5444762
m18150095612 <18150095612@189.cn>
2025-08-10 20:30:26 +0800

    Update center news and site settings.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   title: siteTitle,
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://m18150095612.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/FJTICMIRC/',
